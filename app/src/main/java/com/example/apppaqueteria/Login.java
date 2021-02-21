450e26029ca1fb8573b42ccace02a684b9915a53
Raul Martinez <neskirbi@gmail.com>
2021-02-21 03:23:25 -0600

    Ya consume la api de login

--- app/src/main/java/com/example/apppaqueteria/Login.java
+++ app/src/main/java/com/example/apppaqueteria/Login.java
@@ -2,13 +2,77 @@ package com.example.apppaqueteria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class Login extends AppCompatActivity {
-
+    EditText user,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        user=findViewById(R.id.user);
+        password=findViewById(R.id.password);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+    public void Enviar(View view){
+
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="http://192.168.0.4/Paqueteria/WebPaqueteria/public/api/login/"+user.getText()+"/"+password.getText()+"";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        Log.i("response","Response is: "+ response);
+                        try {
+                            JSONArray jsonArray=new JSONArray(response);
+                            if(jsonArray.length()>0){
+                                JSONObject jsonObject=new JSONObject(jsonArray.get(0).toString());
+                                startActivity(new Intent(getApplicationContext(),MainActivity.class).putExtra("nombre",jsonObject.getString("nombres")+" "+jsonObject.getString("apellidopaterno")));
+                            }else{
+                                Toast.makeText(Login.this, "Error de datos.", Toast.LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("response","That didn't work!"+error);
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
     }
 }
--- app/src/main/java/com/example/apppaqueteria/MainActivity.java
+++ app/src/main/java/com/example/apppaqueteria/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.apppaqueteria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hola "+getIntent().getExtras().getString("nombre"), Toast.LENGTH_SHORT).show();
+
+
     }
+
+
 }
